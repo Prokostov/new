3c05fe880310bc5635092635ce222bc29573ae88
new <kostov@prohost.bg>
2014-04-11 14:04:23 +0200

    Revert "a"
    This reverts commit e6085e6fd40f5644cb09f33ea969005a193f2cb3.

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host="localhost";
-$dbuser="kostov_kostov";
-$dbpass="ednodvetri1";
-$dbname="kostov_db1";
-
-mysql_connect("$host","$dbuser","$dbpass") or die ("Грешка");
-mysql_select_db("$dbname") or die ("Грешка");
-?>
